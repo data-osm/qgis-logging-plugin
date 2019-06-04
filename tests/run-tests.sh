d9c890b7ba6b966934dfe77ebc316c5ff1189633
David Marteau <david@innophi.com>
2019-06-04 13:43:34 +0200

    Fix tests for using in CI environment

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 set -e
+set -x
 
 # Add /.local to path
 export PATH=$PATH:/.local/bin
 
 echo "Installing required packages..."
-pip3 install -q -U --prefer-binary --user -r requirements.txt
+pip3 install -q -U --prefer-binary --user -r tests/requirements.txt
 
 # Disable qDebug stuff that bloats test outputs
 export QT_LOGGING_RULES="*.debug=false;*.warning=false"
 
-py.test -v $@
+pytest -v tests $@
 
